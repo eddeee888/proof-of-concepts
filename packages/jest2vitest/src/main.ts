fe46dc2b761a89190efe59fdde5723087e7baaf3
Eddy Nguyen <enguyen@seek.com.au>
2023-01-27 15:50:57 +1100

    Add missing test function to import from vitest

--- packages/jest2vitest/src/main.ts
+++ packages/jest2vitest/src/main.ts
@@ -3,6 +3,7 @@ import { Project, SyntaxKind } from 'ts-morph';
 const testBasicImports: Record<string, true> = {
   describe: true,
   it: true,
+  test: true,
   expect: true,
   beforeEach: true,
   beforeAll: true,
